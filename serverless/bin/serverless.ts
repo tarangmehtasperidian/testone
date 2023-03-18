af6a861c74a69ff1742758436527b59328f0bb8d
tarangmehtasperidian <74685773+tarangmehtasperidian@users.noreply.github.com>
2023-03-17 18:03:09 -0700

    Delete serverless directory

--- serverless/bin/serverless.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { ServerlessStack } from '../lib/serverless-stack';
-
-const app = new cdk.App();
-new ServerlessStack(app, 'ServerlessStack');

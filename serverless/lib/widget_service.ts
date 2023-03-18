ebb1716b06c8cfc320f5dbf00250b5cce3cc8a82
tarangmehtasperidian <74685773+tarangmehtasperidian@users.noreply.github.com>
2023-03-17 17:45:44 -0700

    Update widget_service.ts

--- serverless/lib/widget_service.ts
+++ serverless/lib/widget_service.ts
@@ -1,20 +1,49 @@
-import * as cdk from 'aws-cdk-lib';
-import {aws_s3 as s3 } from 'aws-cdk-lib'
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as core from "@aws-cdk/core";
+import * as apigateway from "@aws-cdk/aws-apigateway";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as s3 from "@aws-cdk/aws-s3";
 
-export class FirstAppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-     new s3.Bucket(this,'MyFirsrtBucket', {
-        versioned: true,
-        bucketName: 'tarang-cdk-bucket-two'
-     });
-    // The code that defines your stack goes here
+export class WidgetService extends core.Construct {
+    constructor(scope: core.Construct, id: string) {
+        super(scope, id);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FirstAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const bucket = new s3.Bucket(this, "WidgetStore");
+
+        const handler = new lambda.Function(this, "WidgetHandler", {
+            runtime: lambda.Runtime.NODEJS_10_X, // So we can use async in widget.js
+            code: lambda.Code.fromAsset("resources"),
+            handler: "widgets.main",
+            environment: {
+                BUCKET: bucket.bucketName
+            }
+        });
+
+        bucket.grantReadWrite(handler); // was: handler.role);
+
+        const api = new apigateway.RestApi(this, "widgets-api", {
+            restApiName: "Widget Service",
+            description: "This service serves widgets."
+        });
+
+        const getWidgetsIntegration = new apigateway.LambdaIntegration(handler, {
+            requestTemplates: { "application/json": '{ "statusCode": "200" }' }
+        });
+
+        api.root.addMethod("GET", getWidgetsIntegration); // GET /
+
+        const widget = api.root.addResource("{id}");
+
+        // Add new widget to bucket with: POST /{id}
+        const postWidgetIntegration = new apigateway.LambdaIntegration(handler);
+
+        // Get a specific widget from bucket with: GET /{id}
+        const getWidgetIntegration = new apigateway.LambdaIntegration(handler);
+
+        // Remove a specific widget from the bucket with: DELETE /{id}
+        const deleteWidgetIntegration = new apigateway.LambdaIntegration(handler);
+
+        widget.addMethod("POST", postWidgetIntegration); // POST /{id}
+        widget.addMethod("GET", getWidgetIntegration); // GET /{id}
+        widget.addMethod("DELETE", deleteWidgetIntegration); // DELETE /{id}
+    }
 }
